23a912b78ef5fee1b041352f91a8c54124803a6f
Nihal Menon <31966964+nihalmenon@users.noreply.github.com>
2018-05-19 11:17:02 -0500

    updated script

--- js/script.js
+++ js/script.js
@@ -9,8 +9,7 @@ function swapSibling(node1, node2) {
 function addItem(text, checked) {
 	var input = document.querySelector("input");	
 	var inputText = text || input.value;
-	if (inputText.includes(",") || inputText.includes("|")) return false;
-	itemContents.push(inputText);
+	itemContents.push(inputText, checked || 0);
 	
 	if (!inputText && !text) return;
 	
@@ -21,7 +20,7 @@ function addItem(text, checked) {
 	var span = document.createElement("span");
 	span.textContent = inputText;
 	span.setAttribute("contenteditable", "true");
-	
+
 	var upArrow = document.createElement("i");
 	upArrow.className = "up";
 	upArrow.setAttribute("contenteditable", "false");
@@ -34,7 +33,7 @@ function addItem(text, checked) {
 	arrows.appendChild(upArrow);
 	arrows.appendChild(downArrow);
 	arrows.setAttribute("class", "arrows");
-	
+
 	var deleteButton = document.createElement("div");
 	deleteButton.innerHTML = '<svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 26 26" width="50" height="50"><style>svg:hover path { fill: #f00 }</style><path d="M11.5-.031c-1.957 0-3.531 1.625-3.531 3.594V4H4c-.55 0-1 .45-1 1v1H2v2h2v15c0 1.645 1.355 3 3 3h12c1.645 0 3-1.355 3-3V8h2V6h-1V5c0-.55-.45-1-1-1h-3.969v-.438c0-1.968-1.574-3.593-3.531-3.593zm0 2.062h3c.805 0 1.469.656 1.469 1.531V4H10.03v-.438c0-.874.664-1.53 1.469-1.53zM6 8h5.125c.125.012.246.031.375.031h3c.129 0 .25-.02.375-.031H20v15c0 .563-.438 1-1 1H7c-.563 0-1-.438-1-1zm2 2v12h2V10zm4 0v12h2V10zm4 0v12h2V10z" fill="#222"/></svg>';
 
@@ -49,7 +48,8 @@ function addItem(text, checked) {
 	listItem.appendChild(checkbox);
 	listItem.appendChild(span);
 	listItem.appendChild(deleteButton);
-	
+	listItem.appendChild(arrows);
+
 	upArrow.addEventListener('click', function() {
 		if(listItem.previousSibling) list.insertBefore(listItem, listItem.previousSibling);
 	})
@@ -80,10 +80,10 @@ function addItem(text, checked) {
 	});
 
 	span.addEventListener('input', function(e) {
-		var oldText = itemContents[itemIndex - 1];
+		var oldText = itemContents[itemIndex];
 		var checked = document.querySelector("#item" + itemIndex).querySelector("div").textContent == "☐" ? 0 : 1;
 		var text = document.querySelector("#item" + itemIndex + " span").textContent;
-		itemContents[itemIndex - 1] = text;
+		itemContents[itemIndex] = text;
 		localStorage.never4get = localStorage.never4get.replace(oldText + "," + checked, text + "," + checked);
 	});
 
@@ -100,7 +100,6 @@ function keyPress(e) {
 document.addEventListener("DOMContentLoaded", function() {
 	document.querySelector("body>div").addEventListener('click', function(){addItem()});
 	document.querySelector("input").addEventListener('keypress', keyPress);
-
 	localStorage.never4get = localStorage.never4get ||  "Keep using never4get,0|";
 	var items = localStorage.never4get.split("|");
 	for (i = 0; i < items.length; i++) {
@@ -110,3 +109,4 @@ document.addEventListener("DOMContentLoaded", function() {
 	}
 
 });
+
